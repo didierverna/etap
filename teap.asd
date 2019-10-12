f88d1ffc6f6583690d7b4367001253aa5fc56e2b
Didier Verna <didier@didierverna.net>
2019-10-12 12:00:57 +0200

    Initial commit.

---
+++ teap.asd
@@ -0,0 +1,11 @@
+(asdf:defsystem :teap
+  :description "The Typesetting Algorithms Experiment Platform"
+  :author "Didier Verna"
+  :mailto "didier@didierverna.net"
+  :homepage
+  "http://www.lrde.epita.fr/~didier/software/lisp/typesetting.php#teap"
+  :source-control "https://github.com/didierverna/teap"
+  :license "BSD"
+  ;; :version 1.0
+  :depends-on (:mcclim)
+  :components ((:file "teap")))
---
+++ teap.lisp
@@ -0,0 +1,101 @@
+;;; teap.lisp --- Typesetting Algorithms Experiment Platform
+
+;; ==========
+;; Meta Level
+;; ==========
+
+(defpackage :teap
+  (:use :cl)
+  (:export :run))
+
+(in-package :teap)
+
+
+
+;; ===
+;; GUI
+;; ===
+
+(defclass state ()
+  ((kerning :initform nil :accessor kerning)
+   (ligatures :initform nil :accessor ligatures)
+   (hyphenation :initarg nil :accessor hyphenation)
+   (layout :initarg :flush-left :accessor layout)
+   (text :accessor text)))
+
+
+(defclass typesetting-pane (clim:basic-pane)
+  ()
+  (:default-initargs :background clim:+white+))
+
+(clim:define-application-frame teap ()
+  ((state :initform (make-instance 'state) :reader state))
+  (:panes
+   (typesetting
+    (clim:make-pane 'typesetting-pane
+		    :min-width  800
+		    :min-height 300))
+   (source
+    (clim:make-pane 'clim:text-editor
+		    :nlines 10
+		    :ncolumns 60
+		    :value "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+officia deserunt mollit anim id est laborum."
+		    :value-changed-callback
+		    (lambda (pane value)
+		      (declare (ignore pane))
+		      (setf (text (state clim:*application-frame*)) value))))
+   (processing
+    (clim:with-radio-box
+	(:type :some-of
+	 :value-changed-callback
+	 (lambda (pane value &aux (state (state clim:*application-frame*)))
+	   (declare (ignore pane))
+	   (setf (kerning state) nil
+		 (ligatures state) nil
+		 (hyphenation state) nil)
+	   (when (consp value)
+	     (mapc (lambda (gadget)
+		     (case (clim:gadget-id gadget)
+		       (:kerning (setf (kerning state) t))
+		       (:ligatures (setf (ligatures state) t))
+		       (:hyphenation (setf (hyphenation state) t))))
+	       value))))
+      (clim:toggle-button :label "Kerning" :id :kerning)
+      (clim:toggle-button :label "Ligatures" :id :ligatures)
+      (clim:toggle-button :label "Hyphenation" :id :hyphenation)))
+   (layout
+    (clim:with-radio-box
+	(:value-changed-callback
+	 (lambda (pane value)
+	   (declare (ignore pane))
+	   (setf (layout (state clim:*application-frame*))
+		 (clim:gadget-id value))))
+      (clim:radio-box-current-selection
+       (clim:toggle-button :label "Flush left" :id :flush-left))
+      (clim:toggle-button :label "Flush right" :id :flush-right)
+      (clim:toggle-button :label "Justified" :id :justified))))
+  (:layouts
+   (default
+    (clim:vertically ()
+      (clim:horizontally ()
+	(clim:labelling (:label "Processing") processing)
+	(clim:labelling (:label "Layout") layout))
+      (clim:labelling (:label "Source text") source)
+      (:fill (clim:labelling (:label "Typeset text") typesetting))))))
+
+
+
+;; ===========
+;; Entry Point
+;; ===========
+
+(defmacro run ()
+  (clim:run-frame-top-level (clim:make-application-frame 'teap)))
+
+;;; teap.lisp ends here
