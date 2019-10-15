4aea7ec6da11f2c4c0c302e20329d86f968057a1
Didier Verna <didier@didierverna.net>
2019-10-15 14:43:31 +0200

    Add a 20x20 margin before drawing a paragraph.

--- etap.lisp
+++ etap.lisp
@@ -61,10 +61,11 @@
   (set-horizontal-scroll-parameters pane
     :max-range (+ (* paragraph-width zoom) 40))
   (unless (zerop (length paragraph))
-    (gp:with-graphics-scale (pane zoom zoom)
-      (loop :for char-box :across paragraph
-	    :do (gp:draw-character pane (code-char (char-box-char char-box))
-				   (char-box-x char-box) 10)))))
+    (gp:with-graphics-translation (pane 20 20)
+      (gp:with-graphics-scale (pane zoom zoom)
+	(loop :for char-box :across paragraph
+	      :do (gp:draw-character pane (code-char (char-box-char char-box))
+				     (char-box-x char-box) 10))))))
 
 (define-interface etap ()
   ((state :initform (make-instance 'state) :reader state))
