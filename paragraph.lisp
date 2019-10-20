f37c41ac229f67aef8fb54535676107b8bda9a1b
Didier Verna <didier@didierverna.net>
2019-10-20 15:14:13 +0200

    Propagate algorithm and disposition everywhere.

--- paragraph.lisp
+++ paragraph.lisp
@@ -74,7 +74,11 @@
 		   (* (1- (length pinned-lines)) 12)))))
 
 
-(defun lineup-width (lineup glue-length &optional (start 0) end)
+(defun lineup-width (lineup start end &optional (glue-length :natural))
+  (setq glue-length (case glue-length
+		      (:natural #'value)
+		      (:max #'max-length)
+		      (:min #'min-length)))
   (unless end (setq end (length lineup)))
   (loop :with width := 0
 	:for i :from start
@@ -92,22 +96,32 @@
 (defun next-glue-position (lineup &optional (start 0))
   (position-if (lambda (element) (typep element 'glue)) lineup :start start))
 
-(defun line-end (start lineup width glue-length)
+(defun line-end
+    (start lineup width algorithm disposition
+     &aux (glue-length (case algorithm
+			 ((:fixed :best-fit) :natural)
+			 (:first-fit :max)
+			 (:last-fit :min))))
   (loop :for i := (next-glue-position lineup start) :then ii
 	:for ii := (when i (next-glue-position lineup (1+ i)))
-	:for w := (lineup-width lineup glue-length start i) :then (+ w ww)
-	:for ww := (when i (lineup-width lineup glue-length i ii))
+	:for w := (lineup-width lineup start i glue-length) :then (+ w ww)
+	:for ww := (when i (lineup-width lineup i ii glue-length))
 	:while (and i (<= (+ w ww) width))
 	:finally (return i)))
 
 (defun line-boundaries
-    (lineup width glue-length &aux (last (1- (length lineup))))
+    (lineup width algorithm disposition &aux (last (1- (length lineup))))
   (loop :for start := 0 :then (when end (1+ end))
 	:while start
-	:for end := (line-end start lineup width glue-length)
+	:for end := (line-end start lineup width algorithm disposition)
 	:collect (list start (if end (1- end) last))))
 
-(defun create-line (lineup boundary glue-length)
+(defun create-line
+    (lineup boundary width algorithm disposition
+     &aux (glue-length (case algorithm
+			 ((:fixed :best-fit) #'value)
+			 (:first-fit #'max-length)
+			 (:last-fit #'min-length))))
   (make-instance 'line
     :pinned-characters
     (loop :with x := 0
@@ -123,17 +137,15 @@
 	  :else :if (typep element 'glue)
 		  :do (incf x (funcall glue-length element)))))
 
-(defun create-lines (lineup width algorithm
-		     &aux (glue-length (case algorithm
-					 ((:fixed :best-fit) #'value)
-					 (:first-fit #'max-length)
-					 (:last-fit #'min-length))))
-  (mapcar (lambda (boundary) (create-line lineup boundary glue-length))
-    (line-boundaries lineup width glue-length)))
+(defun create-lines (lineup width algorithm disposition)
+  (mapcar (lambda (boundary)
+	    (create-line lineup boundary width algorithm disposition))
+    (line-boundaries lineup width algorithm disposition)))
 
 (defun create-paragraph
     (lineup width algorithm disposition
-     &aux (lines (when lineup (create-lines lineup width algorithm))))
+     &aux (lines (when lineup
+		   (create-lines lineup width algorithm disposition))))
   (make-instance 'paragraph
     :disposition disposition
     :width width
