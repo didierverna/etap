dcac8cbb9a49308e168113a49445e20a187ec3c8
Didier Verna <didier@didierverna.net>
2019-10-24 17:39:58 +0200

    Code cleanup.

--- algorithms/fixed.lisp
+++ algorithms/fixed.lisp
@@ -1,43 +1,36 @@
 (in-package :etap)
 
 (defun fixed-line-end (start lineup width disposition prefer-overfull-lines)
-  (loop :with underfull-end
+  (loop :with underfull-i
 	:with underfull-w
-	:with fit-end
-	:with overfull-end
+	:with fit-i
+	:with overfull-i
 	:with overfull-w
 	:for i := (next-glue-position lineup start) :then ii
 	:for ii := (when i (next-glue-position lineup (1+ i))) ; discard glue
 	:for w := (lineup-width lineup start i) :then (when ww (+ w ww))
 	:for ww := (when i (lineup-width lineup i ii))
-	:while (and w (not overfull-end))
+	:while (and w (not overfull-i))
 	:if (< w width)
-	  :do (setq underfull-end i underfull-w w)
+	  :do (setq underfull-i i underfull-w w)
 	:else :if (= w width)
-		:do (setq fit-end i)
-	:else :do (setq overfull-end i overfull-w w)
-	:finally (return
-		   (cond (fit-end
-			  fit-end)
-			 ((and underfull-w (not overfull-w))
-			  underfull-end)
-			 ((and overfull-w (not underfull-w))
-			  overfull-end)
-			 (t
-			  (case disposition
-			    ((:flush-left :centered :flush-right)
-			     underfull-end)
-			    (:justified
-			     (cond ((< (- width underfull-w)
-				       (- overfull-w width))
-				    underfull-end)
-				   ((< (- overfull-w width)
-				       (- width underfull-w))
-				    overfull-end)
-				   (prefer-overfull-lines
-				    overfull-end)
-				   (t
-				    underfull-end)))))))))
+		:do (setq fit-i i)
+	:else :do (setq overfull-i i overfull-w w)
+	:finally
+	   (return
+	     (cond (fit-i fit-i)
+		   ((and underfull-w (not overfull-w)) underfull-i)
+		   ((and overfull-w (not underfull-w)) overfull-i)
+		   (t
+		    (case disposition
+		      ((:flush-left :centered :flush-right) underfull-i)
+		      (:justified
+		       (cond ((< (- width underfull-w) (- overfull-w width))
+			      underfull-i)
+			     ((< (- overfull-w width) (- width underfull-w))
+			      overfull-i)
+			     (prefer-overfull-lines overfull-i)
+			     (t underfull-i)))))))))
 
 (defmethod create-lines
     (lineup width disposition (algorithm (eql :fixed))
