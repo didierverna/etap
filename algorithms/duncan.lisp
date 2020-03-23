ead44630c2a6952e1832ea7583895422d4ac254c
Didier Verna <didier@didierverna.net>
2020-03-23 16:06:37 +0100

    Fix comment typo.

--- algorithms/duncan.lisp
+++ algorithms/duncan.lisp
@@ -2,7 +2,7 @@
 ;; Page, and Margaret G. Robson, Printing Technology 7, 133-151 (1963).
 
 ;; I don't have the article, but the Knuth-Plass paper gives a description of
-;; it. It searches for an acceptable breaking solutions (that is, with
+;; it. It searches for an acceptable breaking solution (that is, with
 ;; adjustment ratios, that I call lineup-scales <= 1 in abs), while minimizing
 ;; hyphenation. What I don't really know however is how it chooses the final
 ;; solution when there is several possibilities.
