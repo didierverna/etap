283be7edbcee2cee319c5d44e2c8c9594c617a58
Didier Verna <didier@didierverna.net>
2023-02-06 17:21:57 +0100

    Change Fixed algorithm for justification.

--- src/typesetting/paragraph/algorithms/fixed.lisp
+++ src/typesetting/paragraph/algorithms/fixed.lisp
@@ -13,10 +13,11 @@
 ;; equally distant from the paragraph width, the underfull one is chosen,
 ;; unless the "Prefer Overfulls" option is checked.
 
-;; Note that because the inter-word spacing is fixed, there is no difference
-;; between the Flush Left and Justified dispositions (justification is
-;; practically never possible), and the sloppy option has no effect.
-
+;; Note that because the inter-word spacing is fixed, the sloppy option has no
+;; effect. Also, for the same reason, it's practically impossible to justify.
+;; The only difference between the justified disposition and the other ones is
+;; when a fit is found (by miracle): in justified disposition, the "Avoid
+;; Hyphens" option has no effect on fit lines.
 
 (in-package :etap)
 
@@ -45,8 +46,13 @@ lines are equally distant from the paragraph width,
 choose the overfull rather than the underfull one."))
 
 
+;; In order to handle all variants and options, this function starts by
+;; collecting the possible breakpoints, that is, the last word and hyphen
+;; underfulls, a possible miraculous fit, and the first word and hyphen
+;; overfulls. After that, we look at the variants and options, and decide on
+;; what to return from the collected possibilities.
 (defun fixed-line-boundary
-    (lineup start width
+    (lineup start width justification
      &key (variant (car *fixed-variants*)) avoid-hyphens prefer-overfulls)
   "Return the Fixed algorithm's view of the end of line boundary."
   (loop :with underfull :with hyphen-underfull :with word-underfull
@@ -58,6 +64,11 @@ choose the overfull rather than the underfull one."))
 	;; first, and then NIL.
 	:for boundary := (next-boundary lineup start)
 	  :then (next-boundary lineup (stop-idx boundary))
+	;; #### NOTE: we're satisfied to stop at the first word overfull, even
+	;; if we don't have an hyphen overfull, because the "Avoid Hyphens"
+	;; options would have no effect there. On the other hand, if we
+	;; already have an hyphen overfull, it's still important to collect a
+	;; word overfull if possible, because of that very same option.
 	:while (and boundary (not word-overfull))
 	:for w := (lineup-width lineup start (stop-idx boundary))
 	:for hyphenp := (hyphenation-point-p (stop-elt boundary))
@@ -77,67 +88,106 @@ choose the overfull rather than the underfull one."))
 		     (setq word-overfull boundary word-overfull-w w))
 	:finally
 	   (return
-	     (ecase variant
-	       (:underfull
-		(cond ((and fit
-			    (hyphenation-point-p (stop-elt fit))
-			    avoid-hyphens)
-		       (or word-underfull fit))
-		      (fit fit)
-		      (underfull
-		       (if avoid-hyphens
-			 (or word-underfull underfull)
-			 underfull))
-		      (t overfull)))
-	       (:overfull
-		(cond ((and fit
-			    (hyphenation-point-p (stop-elt fit))
-			    avoid-hyphens)
-		       (or word-overfull fit))
-		      (fit fit)
-		      (overfull
-		       (if avoid-hyphens
-			 (or word-overfull overfull)
-			 overfull))
-		      (t underfull)))
-	       (:best
-		(cond (fit fit)
+	     (if justification
+	       (if fit
+		 fit
+		 (ecase variant
+		   (:underfull (or underfull overfull))
+		   (:overfull (or overfull underfull))
+		   (:best
+		    (cond
+		      ;; Only one possibility, so no choice.
 		      ((and underfull (not overfull))
 		       underfull)
 		      ((and overfull (not underfull))
 		       overfull)
+		      ;; Two possibilities, but one is closer to the
+		      ;; paragraph's width, so still no choice.
+		      ((< (- width underfull-w) (- overfull-w width))
+		       underfull)
+		      ((> (- width underfull-w) (- overfull-w width))
+		       overfull)
+		      ;; Now we have equidistance.
+		      ;; If we have two, or no hyphen, the "Avoid Hyphens"
+		      ;; option has no effect, but we might still prefer
+		      ;; overfulls.
+		      ((or (and (eq underfull hyphen-underfull)
+				(eq overfull hyphen-overfull))
+			   (and (eq underfull word-underfull)
+				(eq overfull word-overfull)))
+		       (if prefer-overfulls overfull underfull))
+		      ;; Now we know we have exactly one hyphen.
+		      ;; If we care, choose the other solution.
+		      (avoid-hyphens
+		       (if (eq underfull hyphen-underfull)
+			 overfull
+			 underfull))
+		      ;; Finally, we might still prefer overfulls.
 		      (t
-		       (flet ((best-*full (underfull underfull-w
-					   overfull overfull-w)
-				(cond ((< (- width underfull-w)
-					  (- overfull-w width))
-				       underfull)
-				      ((< (- overfull-w width)
-					  (- width underfull-w))
-				       overfull)
-				      (prefer-overfulls overfull)
-				      (t underfull))))
+		       (if prefer-overfulls overfull underfull))))))
+	       (ecase variant
+		 (:underfull
+		  (cond ((and fit
+			      (hyphenation-point-p (stop-elt fit))
+			      avoid-hyphens)
+			 (or word-underfull fit))
+			(fit fit)
+			(underfull
 			 (if avoid-hyphens
-			   (cond ((and word-underfull (not word-overfull))
-				  word-underfull)
-				 ((and (not word-underfull) word-overfull)
-				  word-overfull)
-				 ((and word-underfull word-overfull)
-				  (best-*full word-underfull word-underfull-w
-					      word-overfull word-overfull-w))
-				 (t
-				  (best-*full underfull underfull-w
-					      overfull overfull-w)))
-			   (best-*full
-			    underfull underfull-w
-			    overfull overfull-w))))))))))
+			   (or word-underfull underfull)
+			   underfull))
+			(t overfull)))
+		 (:overfull
+		  (cond ((and fit
+			      (hyphenation-point-p (stop-elt fit))
+			      avoid-hyphens)
+			 (or word-overfull fit))
+			(fit fit)
+			(overfull
+			 (if avoid-hyphens
+			   (or word-overfull overfull)
+			   overfull))
+			(t underfull)))
+		 (:best
+		  (cond (fit fit)
+			((and underfull (not overfull))
+			 underfull)
+			((and overfull (not underfull))
+			 overfull)
+			(t
+			 (flet ((best-*full (underfull underfull-w
+					     overfull overfull-w)
+				  (cond ((< (- width underfull-w)
+					    (- overfull-w width))
+					 underfull)
+					((< (- overfull-w width)
+					    (- width underfull-w))
+					 overfull)
+					(prefer-overfulls overfull)
+					(t underfull))))
+			   (if avoid-hyphens
+			     (cond ((and word-underfull (not word-overfull))
+				    word-underfull)
+				   ((and (not word-underfull) word-overfull)
+				    word-overfull)
+				   ((and word-underfull word-overfull)
+				    (best-*full word-underfull word-underfull-w
+						word-overfull word-overfull-w))
+				   (t
+				    (best-*full underfull underfull-w
+						overfull overfull-w)))
+			     (best-*full
+			      underfull underfull-w
+			      overfull overfull-w)))))))))))
 
 (defmethod make-lines
     (lineup disposition width (algorithm (eql :fixed))
-     &rest keys &key variant avoid-hyphens prefer-overfulls)
+     &rest keys &key variant avoid-hyphens prefer-overfulls
+     &aux (justification (eq (disposition-type disposition) :justified)))
   "Typeset LINEUP with the Fixed algorithm."
   (declare (ignore variant avoid-hyphens prefer-overfulls))
   (loop :for start := 0 :then (next-start boundary)
 	:while start
-	:for boundary := (apply #'fixed-line-boundary lineup start width keys)
+	:for boundary := (apply #'fixed-line-boundary
+			   lineup start width justification keys)
 	:collect (make-line lineup start (stop-idx boundary))))
