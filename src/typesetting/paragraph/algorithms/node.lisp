01816ee7b325331ba2d3ef2bfc291cc10aaa61e6
Didier Verna <didier@didierverna.net>
2023-02-23 18:43:09 +0100

    Some docstrings in node.lisp.

--- src/typesetting/paragraph/algorithms/node.lisp
+++ src/typesetting/paragraph/algorithms/node.lisp
@@ -1,35 +1,46 @@
 ;; #### NOTE: with the defaults (default text, 284pt, all features), there are
-;; #### 66576 paragraph solutions including going through under and overfull
-;; #### lines (21096 without hyphenation). The raw tree of all such solutions
-;; #### has 150860 nodes (48338 without hyphenation). However, once a line
-;; #### stop has been decided, all possible solutions for the next lines
-;; #### remain the same (modulo the rectangular paragraph assumption), however
-;; #### we reached that possible stop. This means that there is a lot of room
-;; #### for re-using branches. And indeed, when sharing nodes, we fall from
-;; #### 150860 to 98 (from 48338 to 83 without hyphenation).
+;; 66576 paragraph solutions including going through under and overfull lines
+;; (21096 without hyphenation). The raw tree of all such solutions #### has
+;; 150860 nodes (48338 without hyphenation). However, once a line #### stop
+;; has been decided, all possible solutions for the next lines #### remain the
+;; same (modulo the rectangular paragraph assumption), however #### we reached
+;; that possible stop. This means that there is a lot of room #### for
+;; re-using branches. And indeed, when sharing nodes, we fall from #### 150860
+;; to 98 (from 48338 to 83 without hyphenation).
 
-;; #### If we avoid preventive fulls, that is, if we include only under- and
-;; #### overfull solutions when there is not fit, the number of paragraph
-;; #### solutions falls to 37 (it actually raises up to 61 without
-;; #### hyphenation, all mistfits). The raw tree of all such solutions has
-;; #### only 109 nodes (192 without hyphenations). Once shared, the actual
-;; #### number of nodes falls down to 30 (33 without hyphenation).
+;; If we avoid preventive fulls, that is, if we include only under- and ####
+;; overfull solutions when there is not fit, the number of paragraph ####
+;; solutions falls to 37 (it actually raises up to 61 without ####
+;; hyphenation, all mistfits). The raw tree of all such solutions has ####
+;; only 109 nodes (192 without hyphenations). Once shared, the actual ####
+;; number of nodes falls down to 30 (33 without hyphenation).
 
 (in-package :etap)
 
 
 (defclass paragraph-edge ()
-  ((node :initarg :node :reader node)))
+  ((node :documentation "The node this edge points to."
+	 :initarg :node :reader node))
+  (:documentation "The PARAGRAPH-EDGE class."))
+
 
 (defclass paragraph-node ()
-  ((boundary :initarg :boundary :reader boundary)
-   (edges :initarg :edges :reader edges)))
+  ((boundary :documentation "This node's boundary."
+	     :initarg :boundary :reader boundary)
+   (edges :documentation "The edges from this node to other nodes."
+	  :initarg :edges :reader edges))
+  (:documentation "The PARAGRAPH-NODE class."))
 
 (defun make-paragraph-node (boundary edges)
+  "Make a paragraph node out of BOUNDARY and EDGES."
   (make-instance 'paragraph-node :boundary boundary :edges edges))
 
+
 (defgeneric next-boundaries
-    (lineup start width child-type &key &allow-other-keys))
+    (lineup start width child-type &key &allow-other-keys)
+  (:documentation
+   "Find the possible next boundaries from START position in LINEUP."))
+
 
 ;; #### WARNING: we use a hash table for storing and sharing nodes to express
 ;; the fact that however we reach a break, all subsequent solutions for the
@@ -40,6 +51,9 @@
 ;; need to take the line number as well as the boundary into account.
 (defun create-paragraph-node
     (lineup width algorithm-type edge-type boundary hash &rest options)
+  "Create a node representing ALGORITHM-TYPE's view a line breaking solution
+for a LINEUP paragraph of WIDTH starting at BOUNDARY. If no line breaking
+solution is found, this function returns NIL."
   (or (gethash (stop-idx boundary) hash)
       (setf (gethash (stop-idx boundary) hash)
 	    (if (= (stop-idx boundary) (length lineup))
@@ -77,6 +91,13 @@
 			       next-boundary hash
 			       options)
 			 :collect :it)))
+  "Create a graph representing a LINEUP paragraph of WIDTH with ALGORITHM-TYPE.
+The graph contains the algorithm's view of all line break solutions, that is,
+all possible routes from one boundary to another.
+If no line breaking solution is found, this function returns NIL.
+Otherwise, it returns the first node, that is, the node representing the start
+of the paragraph. Thus, this node contains a null boundary and the edges
+pointing to the first possible breaks."
   (when nodes
     (make-paragraph-node
      nil
