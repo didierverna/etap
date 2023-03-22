8a5cc313e337eba6f1b29ca625d4dc0cb1eb437c
Didier Verna <didier@didierverna.net>
2023-03-22 17:02:05 +0100

    Experiments improvements.

--- src/experiments/graph.lisp
+++ src/experiments/graph.lisp
@@ -1,6 +1,19 @@
 (in-package :etap)
 
-(defun graph-statistics ()
+(defun graph-solutions ()
+  "Collect and print the number of solutions per paragraph width.
+Numbers are collected for every paragraph width from *PARAGRAPH-MIN-WIDTH*
+to *PARAGRAPH-MAX-WIDTH*, and for each of the four categories below:
+- no hyphens, justified last line,
+- hyphens, justified last line,
+- no hyphens, underfull last line,
+- hyphens, underfull last line.
+
+The output, suitable to Gnuplot, is of the following form:
+Width Strict Strict/Hyphens Regular Regular/Hyphens
+width1 strict1 strict/hyphens1 regular1 regular/hyphens1
+width2 strict2 strict/hyphens2 regular2 regular/hyphens2
+..."
   (let* ((*context* *experiments-context*)
 	 (lineup (make-lineup :hyphenation nil))
 	 (hyphenated-lineup (make-lineup)))
