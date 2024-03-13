77ca5bddc33ef86e7e645d27066f20fbaa9ef343
Didier Verna <didier@didierverna.net>
2024-03-13 16:34:54 +0100

    Nickname the TFM package differently.
    2024-03-13  Didier Verna  <didier@didierverna.net>
    
    	* src/meta.lisp (:etap): Use the package local nicknames feature
    	for TFM.

--- src/meta.lisp
+++ src/meta.lisp
@@ -1,10 +1,8 @@
 (in-package :cl-user)
 
-;; #### FIXME: this is NOT the way to do it!
-(net.didierverna.tfm:nickname-package)
-
 (defpackage :etap
   #+lispworks (:add-use-defaults t)
+  (:local-nicknames (:tfm :net.didierverna.tfm))
   (:use #+lispworks :capi #-lispworks :cl)
   (:shadow :$+ :$/
    ;; CAPI exports these and it sucks.
