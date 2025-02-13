1bb86664f06d84df37002b00e1835cbd2bfd77b8
Didier Verna <didier@didierverna.net>
2025-02-13 10:41:30 +0100

    Prepare the PINNED class to be used concretely.
    2025-02-13  Didier Verna  <didier@didierverna.net>
    
    	* src/typesetting/common.lisp (pinned): New OBJECT slot. Convert
    	accessors to readers.

--- src/typesetting/common.lisp
+++ src/typesetting/common.lisp
@@ -1,12 +1,18 @@
 (in-package :etap)
 
 (defclass pinned ()
-  ((board :documentation "The object's board."
-	  :initform nil :initarg :board :reader board)
-   (x :documentation "The object's X coordinate, relative to its board."
-      :initform 0 :initarg :x :accessor x)
-   (y :documentation "The object's Y coordinate, relative to its board."
-      :initform 0 :initarg :y :accessor y))
+  ((object
+    :documentation "The pinned object."
+    :initarg :object :reader object)
+   (board
+    :documentation "The pinned object's board."
+    :initform nil :initarg :board :reader board)
+   (x
+    :documentation "The pinned object's X coordinate, relative to its board."
+    :initform 0 :initarg :x :reader x)
+   (y
+    :documentation "The pinned object's Y coordinate, relative to its board."
+    :initform 0 :initarg :y :reader y))
   (:documentation "The PINNED class.
-This is the base class for all pinned objects, that is, objects which have a
-fixed position in 2D space, expressed relative to their board."))
+This class represents objects which are pinned to a fixed position in 2D
+space, expressed relative to their board."))
