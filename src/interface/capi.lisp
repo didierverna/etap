2dcb145c6301bc1e00037eee9d6f5ff7a8b845f3
Didier Verna <didier@didierverna.net>
2025-06-23 22:02:40 +0200

    Code cleanup.
    2025-06-23  Didier Verna  <didier@didierverna.net>
    
    	* src/interface/capi.lisp: Move some code around.
    	(motion-callback): New name for DISPLAY-PROPERTIES.
    	(post-menu-callback): New name for CONTEXTUAL-MENU.
    	(etap): Update accordingly.

--- src/interface/capi.lisp
+++ src/interface/capi.lisp
@@ -16,6 +16,64 @@
   `(calibrate gui ,name :earmuffs nil))
 
 
+;; --------------------------------------
+;; CLIM-less object under mouse detection
+;; --------------------------------------
+
+(defun vector-product (p1 p2 p3)
+  "Return the vector product of P1P2 - P1P3.
+Each point is of the form (X . Y)."
+  (let ((x1 (car p1)) (y1 (cdr p1))
+	(x2 (car p2)) (y2 (cdr p2))
+	(x3 (car p3)) (y3 (cdr p3)))
+    (- (* (- x1 x3) (- y2 y3)) (* (- x2 x3) (- y1 y3)))))
+
+(defun triangle-under-p (p a b c)
+  "Return T if P is within the ABC triangle."
+  (let ((vp1 (vector-product p a b))
+	(vp2 (vector-product p b c))
+	(vp3 (vector-product p c a)))
+    (not (or (and (< vp1 0) (>= vp2 0) (>= vp3 0))
+	     (and (< vp2 0) (>= vp1 0) (>= vp3 0))
+	     (and (< vp3 0) (>= vp1 0) (>= vp2 0))
+	     (and (> vp1 0) (<= vp2 0) (<= vp3 0))
+	     (and (> vp2 0) (<= vp1 0) (<= vp3 0))
+	     (and (> vp3 0) (<= vp1 0) (<= vp2 0))))))
+
+(defun hyphenation-point-under (x y lines &aux (p (cons x y)))
+  "Return the hyphenation point from LINES which is under (X, Y), or nil.
+Technically, (X, Y) is not over the hyphenation point, but over the
+corresponding hyphenation clue."
+  (let ((line (find-if (lambda (line)
+			 (and (>= y (y line)) (<= y (+ (y line) 5))))
+		       lines)))
+    (when line
+      (let* ((x (x line))
+	     (y (y line))
+	     (pinned (find-if (lambda (item)
+				(and (discretionary-clue-p (object item))
+				     (hyphenation-point-p
+				      (discretionary (object item)))
+				     (triangle-under-p
+				      p
+				      (cons (+ x (x item)) y)
+				      (cons (+ x (x item) -3) (+ y 5))
+				      (cons (+ x (x item) +3) (+ y 5)))))
+			      (items line))))
+	(when pinned (discretionary (object pinned)))))))
+
+(defun line-under (y lines)
+  "Return the line from LINES which is under Y coordinate, or NIL."
+  (find-if (lambda (line)
+	     (and (>= y (- (y line) (height line)))
+		  (<= y (+ (y line) (depth line)))))
+	   lines))
+
+
+;; --------
+;; Updaters
+;; --------
+
 (defun remake-rivers
     (interface
      &aux (rivers-interface (rivers-interface interface))
@@ -596,60 +654,11 @@ through 0 (green), and finally to +∞ (red)."
        (symbol (cadr (member key *tooltips*)))))))
 
 
-;; ----------
-;; Properties
-;; ----------
+;; ---------------
+;; Motion Callback
+;; ---------------
 
-(defun vector-product (p1 p2 p3)
-  "Return the vector product of P1P2 - P1P3.
-Each point is of the form (X . Y)."
-  (let ((x1 (car p1)) (y1 (cdr p1))
-	(x2 (car p2)) (y2 (cdr p2))
-	(x3 (car p3)) (y3 (cdr p3)))
-    (- (* (- x1 x3) (- y2 y3)) (* (- x2 x3) (- y1 y3)))))
-
-(defun triangle-under-p (p a b c)
-  "Return T if P is within the ABC triangle."
-  (let ((vp1 (vector-product p a b))
-	(vp2 (vector-product p b c))
-	(vp3 (vector-product p c a)))
-    (not (or (and (< vp1 0) (>= vp2 0) (>= vp3 0))
-	     (and (< vp2 0) (>= vp1 0) (>= vp3 0))
-	     (and (< vp3 0) (>= vp1 0) (>= vp2 0))
-	     (and (> vp1 0) (<= vp2 0) (<= vp3 0))
-	     (and (> vp2 0) (<= vp1 0) (<= vp3 0))
-	     (and (> vp3 0) (<= vp1 0) (<= vp2 0))))))
-
-(defun hyphenation-point-under (x y lines &aux (p (cons x y)))
-  "Return the hyphenation point from LINES which is under (X, Y), or nil.
-Technically, (X, Y) is not over the hyphenation point, but over the
-corresponding hyphenation clue."
-  (let ((line (find-if (lambda (line)
-			 (and (>= y (y line)) (<= y (+ (y line) 5))))
-		       lines)))
-    (when line
-      (let* ((x (x line))
-	     (y (y line))
-	     (pinned (find-if (lambda (item)
-				(and (discretionary-clue-p (object item))
-				     (hyphenation-point-p
-				      (discretionary (object item)))
-				     (triangle-under-p
-				      p
-				      (cons (+ x (x item)) y)
-				      (cons (+ x (x item) -3) (+ y 5))
-				      (cons (+ x (x item) +3) (+ y 5)))))
-			      (items line))))
-	(when pinned (discretionary (object pinned)))))))
-
-(defun line-under (y lines)
-  "Return the line from LINES which is under Y coordinate, or NIL."
-  (find-if (lambda (line)
-	     (and (>= y (- (y line) (height line)))
-		  (<= y (+ (y line) (depth line)))))
-	   lines))
-
-(defun display-properties
+(defun motion-callback
     (pane x y
      &aux (interface (top-level-interface pane))
 	  (zoom (/ (range-slug-start (zoom interface)) 100))
@@ -686,14 +695,14 @@ corresponding hyphenation clue."
 	    (display-tooltip pane)))))))
 
 
-;; ---------------
-;; Contextual Menu
-;; ---------------
+;; ------------------
+;; Post Menu Callback
+;; ------------------
 
 ;; #### TODO: when this gets enriched, we will eventually end up with the same
-;; logic as in DISPLAY-PROPERTIES in order to figure out what's under the
-;; mouse, and we already wish we used CLIM...
-(defun contextual-menu
+;; logic as in MOTION-CALLBACK in order to figure out what's under the mouse,
+;; and we already wish we used CLIM...
+(defun post-menu-callback
     (pane x y
      &aux (interface (top-level-interface pane))
 	  (zoom (/ (range-slug-start (zoom interface)) 100))
@@ -1084,8 +1093,8 @@ corresponding hyphenation clue."
      :vertical-scroll t
      :display-callback 'render-view
      :reader view
-     :input-model '((:motion display-properties)
-		    (:post-menu contextual-menu))))
+     :input-model '((:motion motion-callback)
+		    (:post-menu post-menu-callback))))
   (:layouts
    (main column-layout '(settings view))
    (settings row-layout '(settings-1 settings-2))
