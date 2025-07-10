c235320a024472a182e19cbf1936d49b64156a65
Didier Verna <didier@didierverna.net>
2025-07-10 18:46:46 +0200

    Fix RENDER-LAYOUT bug.
    2025-07-10  Didier Verna  <didier@didierverna.net>
    
    	* src/typesetting/paragraph/algorithms/common.lisp
    	(render-layout): Update for recent breakup / lineup data structure
    	changes.

--- src/typesetting/paragraph/algorithms/common.lisp
+++ src/typesetting/paragraph/algorithms/common.lisp
@@ -475,24 +475,27 @@ specific global properties."))
   ()
   (:documentation "The LINNED-LINE class."))
 
-(defun render-layout (layout &aux (lines (lines layout)))
+(defun render-layout
+    (layout
+     &aux (lines (lines layout))
+	  (breakup (breakup layout))
+	  (par-width (paragraph-width breakup)))
   "Render LAYOUT's lines and pin them. Return LAYOUT."
   (when lines
-    (with-slots (harray disposition width) (breakup layout)
-      (loop :with class := (pinned-line-class layout)
-	    :with baseline-skip := (baseline-skip harray)
-	    :with x := (ecase (disposition-type disposition)
-			 ((:flush-left :justified)
-			  (lambda (line) (declare (ignore line)) 0))
-			 (:centered
-			  (lambda (line) (/ (- width (width line)) 2)))
-			 (:flush-right
-			  (lambda (line) (- width (width line)))))
-	    :for y := 0 :then (+ y baseline-skip)
-	    :for line :in lines
-	    :do (render-line line)
-	    :do (change-class line class
-		  :board layout :x (funcall x line) :y y))))
+    (loop :with class := (pinned-line-class layout)
+	  :with baseline-skip := (baseline-skip (harray breakup))
+	  :with x := (ecase (disposition-type (disposition breakup))
+		       ((:flush-left :justified)
+			(lambda (line) (declare (ignore line)) 0))
+		       (:centered
+			(lambda (line) (/ (- par-width (width line)) 2)))
+		       (:flush-right
+			(lambda (line) (- par-width (width line)))))
+	  :for y := 0 :then (+ y baseline-skip)
+	  :for line :in lines
+	  :do (render-line line)
+	  :do (change-class line class
+		:board layout :x (funcall x line) :y y)))
   layout)
 
 ;; #### NOTE: nothing prevents an algorithm from sharing an object
