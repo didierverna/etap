a716c6d3cbfdfb82027a264f726344c034feeeb1
Didier Verna <didier@didierverna.net>
2025-09-24 12:39:34 +0200

    Export more functionality.
    2025-09-24  Didier Verna  <didier@didierverna.net>
    
    	* src/meta.lisp (:etap): Update export symbol list.

--- src/meta.lisp
+++ src/meta.lisp
@@ -13,7 +13,22 @@
    ;; From the CAPI package (this sucks):
    :item :layout)
   (:export
-    :*language* :*text* :*paragraph-width*
-    :*context* :make-context
-    :make-lineup :make-breakup
-    #+lispworks :state #+lispworks :set-state #+lispworks :run))
+    ;; From font/font.lisp:
+    :*font-file* :*font*
+    ;; From language/hyphenation.lisp:
+    :*lefthyphenmin* :*righthyphenmin*
+    ;; From language/language.lisp:
+    :*language*
+    ;; From language/text.lisp:
+    :*text*
+    ;; From typesetting/paragraph/common.lisp:
+    :disposition-type :disposition-options
+    ;; From typesetting/paragraph/algorithms/common.lisp:
+    :algorithm-type :algorithm-options
+    ;; From context.lisp:
+    :font :algorithm :disposition :features :paragraph-width :text :language
+    :make-context :*context*
+    ;; From interface/capi.lisp:
+    #+lispworks :state #+lispworks :set-state #+lispworks :run
+    ;; From entry.lisp:
+    :make-lineup :make-breakup))
