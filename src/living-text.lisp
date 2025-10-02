85037d5cd1a7550bda01ee0ec8464b74e25315ef
Didier Verna <didier@didierverna.net>
2025-10-02 10:54:12 +0900

    Abstract demo-1 code away.
    2025-10-02  Didier Verna  <didier@didierverna.net>
    
    	* src/living-text.lisp (line-x-shift):
    	(line-y-shift):
    	(char-y-shift):
    	(char-y-shift): Remove.
    	(demo-1): New struct. Handle both horizontal and vertical moves at
    	the same time.
    	(demo-1-step): Use it.
    	(demo-1-initialize): Take a DEMO-1 struct as argument.
    	(demo-1): Accept x and y increments at the same time.

--- src/living-text.lisp
+++ src/living-text.lisp
@@ -1,37 +1,52 @@
 (in-package :etap)
 
-(defvar line-x-shift (lambda (line) 0))
-(defvar line-y-shift (lambda (line) 0))
-(defvar char-x-shift (lambda (item) 0))
-(defvar char-y-shift (lambda (item) 0))
-
+(defstruct demo-1 xinc yinc xshift yshift)
 
 (defun demo-1-step (pane)
   (cond ((eq (capi-object-property pane :stop) t)
+	 (setf (capi-object-property pane :demo-1) nil)
 	 (setf (capi-object-property pane :line-x-shift) nil)
+	 (setf (capi-object-property pane :line-y-shift) nil)
 	 (redisplay-element pane)
 	 (setf (capi-object-property pane :stop) nil)
 	 :stop)
 	(t
-	 (setf (capi-object-property pane :shift)
-	       (mod (1+ (capi-object-property pane :shift)) 200))
+	 (when (demo-1-xinc (capi-object-property pane :demo-1))
+	   (setf (demo-1-xshift (capi-object-property pane :demo-1))
+		 (mod (+ (demo-1-xinc (capi-object-property pane :demo-1))
+			 (demo-1-xshift (capi-object-property pane :demo-1)))
+		      200)))
+	 (when (demo-1-yinc (capi-object-property pane :demo-1))
+	   (setf (demo-1-yshift (capi-object-property pane :demo-1))
+		 (mod (+ (demo-1-yinc (capi-object-property pane :demo-1))
+			 (demo-1-yshift (capi-object-property pane :demo-1)))
+		      200)))
 	 (redisplay-element pane))))
 
-(defun demo-1-initialize (pane axis)
-  (setf (capi-object-property pane :shift) 0)
-  (setf (capi-object-property
-	 pane (case axis (:x :line-x-shift) (:y :line-y-shift)))
+(defun demo-1-initialize (pane demo-1)
+  (setf (capi-object-property pane :demo-1) demo-1)
+  (when (demo-1-xinc demo-1)
+    (setf (capi-object-property pane :line-x-shift)
 	(lambda (line)
 	  (if line
-	    (+ 5 (* 5 (sin (+ (/ (* (capi-object-property pane :shift) pi)
+	    (+ 5 (* 5 (sin (+ (/ (* (demo-1-xshift (capi-object-property pane :demo-1)) pi)
 				 100)
-			      (y line))))))))
+			      (y line)))))))))
+  (when (demo-1-yinc demo-1)
+    (setf (capi-object-property pane :line-y-shift)
+	(lambda (line)
+	  (if line
+	    (+ 5 (* 5 (sin (+ (/ (* (demo-1-yshift (capi-object-property pane :demo-1)) pi)
+				 100)
+			      (y line)))))))))
   (mp:schedule-timer-relative-milliseconds
    (mp:make-timer 'demo-1-step pane) 10 10))
 
 
-(defun demo-1 (etap &optional (axis :x))
-  (execute-with-interface-if-alive etap 'demo-1-initialize (view etap) axis))
+(defun demo-1 (etap &rest keys &key xinc yinc)
+  (declare (ignore xinc yinc))
+  (execute-with-interface-if-alive etap 'demo-1-initialize
+    (view etap) (apply #'make-demo-1 :xshift 0 :yshift 0 keys)))
 
 ;; #### WARNING: STOP is executed asynchronously, so it's not a good idea to
 ;; mess around with any property that the demos may use. Hence a specific STOP
