370ad70ef742eb5b0a600a6aef7be11bf21a8467
Didier Verna <didier@didierverna.net>
2025-12-28 19:24:24 +0100

    Demo functions for changing font.
    2025-12-28  Didier Verna  <didier@didierverna.net>
    
    	* src/user.lisp (bold):
    	(italic):
    	(bold-italic): New functions.
    	(:etap-user): Export them.

--- src/user.lisp
+++ src/user.lisp
@@ -1,4 +1,20 @@
 (in-package :etap)
 (in-readtable :etap)
 
-(defpackage :etap-user (:use :cl :etap))
+(defpackage :etap-user
+  (:use :cl :etap)
+  (:export :bold :italic :bold-italic))
+
+(in-package :etap-user)
+
+(defun bold (string)
+  "Typeset STRING in bold font."
+  (let ((*font* (first (second *fonts*)))) (etap::slice string)))
+
+(defun italic (string)
+  "Typeset STRING in italic font."
+  (let ((*font* (first (third *fonts*)))) (etap::slice string)))
+
+(defun bold-italic (string)
+  "Typeset STRING in bold italic font."
+  (let ((*font* (first (fourth *fonts*)))) (etap::slice string)))
